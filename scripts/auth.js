329a0ae700fbf1b9b3268f1a30cc88a3f46c65fe
Nicolas Montero <nicolasmontero42@gmail.com>
2020-11-08 22:11:53 -0500

    Form and store styles update

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,8 +7,11 @@ const authSignout = document.querySelector('.signout');
 var userInfo;
 
 firebase.auth().onAuthStateChanged(function (user) {
+
   if (user) {
     // si el usuario existe quiere decir que inició sesión, se registró o ya tenía sesión iniciada
+
+    console.log(user);
     authWith.classList.remove('hidden');
     authWithout.classList.add('hidden');
 
@@ -18,33 +21,23 @@ firebase.auth().onAuthStateChanged(function (user) {
       if (doc.exists) {
         const data = doc.data();
         userInfo = data;
-        authProfileSpan.innerText = data.firstname;
 
-        /*  const url = `profile.html?${data.id}`;
-          authProfile.setAttribute('href',url); */
-
-        /*if(data.admin) {
+        if (data.admin) {
           const showAdmin = document.querySelectorAll('.showadmin');
-          showAdmin.forEach(function(elem) {
+          showAdmin.forEach(function (elem) {
             elem.classList.remove('hidden');
           });
-        }*/
+        }
       }
     });
-
-  } else {
-    // si no existe quiere decir que no ha iniciado sesión o acaba de cerrar sesión
-    authWith.classList.add('hidden');
-    authWithout.classList.remove('hidden');
-
-  }
+  } 
 });
 
 // cerrar sesión
 authSignout.addEventListener('click', function (event) {
-  
+
   console.log("aaaaaaaaa")
-  
+
   event.preventDefault();
   firebase.auth().signOut();
   window.location.href = 'login.html';
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const db = firebase.firestore();
+const usersRef = db.collection('users');
+
+var storageRef = firebase.storage().ref();
