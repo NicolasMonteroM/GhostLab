3e01b02c6d19112491585f828958991d8730e965
Nicolas Montero <nicolasmontero42@gmail.com>
2020-12-11 03:32:16 -0500

    Small microinteraction added
    classes updated

--- scripts/products.js
+++ scripts/products.js
@@ -9,7 +9,6 @@ const productsList = document.querySelector('.products');
 let selectedItem = null;
 const imagePath = [];
 
-
 function renderProducts(list) {
     productsList.innerHTML = '';
     list.forEach(function (elem) {
@@ -29,6 +28,7 @@ function renderProducts(list) {
             </a>
             </div>
             <div class="products__preview icon">
+                <p class="products__addText">Add product</p>
                 <svg width="32" height="32" viewBox="0 0 79 89" width:"32" height:"32" xmlns="http://www.w3.org/2000/svg">
                 <path class="addToBag bagIcon showuser" fill-rule="evenodd" clip-rule="evenodd" d="M73.6214 23.2536L78.711 86.2372C78.7298 86.3577 78.7394 86.4815 78.7394 86.6075C78.7394 87.9291 77.6685 89 76.3469 89H76.3406H2.65239C1.98473 89 1.34674 88.7206 0.893761 88.2296C0.440786 87.7387 0.213661 87.0809 0.267571 86.4149L5.37151 23.2536C5.472 22.0111 6.50969 21.0538 7.75633 21.0538H21.2075V18.0249C21.2075 8.08592 29.3996 0 39.3386 0H39.6544C49.5937 0 57.573 8.08592 57.573 18.0249V21.0538H71.2366C72.4833 21.0538 73.521 22.0111 73.6214 23.2536ZM39.3386 4.78495C32.038 4.78495 25.9924 10.7243 25.9924 18.0249V21.0538H52.7881V18.0249C52.7881 10.7243 46.9549 4.78495 39.6544 4.78495H39.3386ZM41.5 44C41.5 42.8954 40.6046 42 39.5 42C38.3954 42 37.5 42.8954 37.5 44V55.5H26C24.8954 55.5 24 56.3954 24 57.5C24 58.6046 24.8954 59.5 26 59.5H37.5V71C37.5 72.1046 38.3954 73 39.5 73C40.6046 73 41.5 72.1046 41.5 71V59.5H53C54.1046 59.5 55 58.6046 55 57.5C55 56.3954 54.1046 55.5 53 55.5H41.5V44Z"/>
                 </svg>
@@ -64,9 +64,9 @@ function renderProducts(list) {
         deleteBtn.addEventListener('click', function () {
 
             productsRef.doc(elem.id).delete().then(function () {
-                    console.log("Document successfully deleted!");
-                    getProducts();
-                })
+                console.log("Document successfully deleted!");
+                getProducts();
+            })
                 .catch(function (error) {
                     console.error("Error removing document: ", error);
                 });
@@ -83,8 +83,6 @@ function renderProducts(list) {
 
         });
 
-        //ADD TO CART
-
         const addBtn = newProduct.querySelector('.addToBag');
         addBtn.addEventListener('click', function (event) {
             event.preventDefault();
@@ -98,13 +96,19 @@ function renderProducts(list) {
                         tech: elem.tech,
                         price: elem.price,
                         largeInfo: elem.largeInfo,
-     
+
                     }
 
                     bagProductRef.set(bagProduct);
                 }
             });
         });
+
+/*
+        gsap.to('.addToBag', {
+            opacity: 1,
+            duration: 5
+        });*/
 
         productsList.appendChild(newProduct);
 
@@ -115,7 +119,7 @@ function renderProducts(list) {
 let objectsList = [];
 
 function getProducts() {
-    productsRef.get() // pide todos los documentos de la colección
+    productsRef.get()
         .then((querySnapshot) => {
             objectsList = [];
             querySnapshot.forEach((doc) => {
@@ -241,14 +245,15 @@ filterForm.addEventListener('input', function () {
     }
 
     const techfilter = filterForm.techfilter.value;
-  
+
     if (techfilter != '') {
-      copy = copy.filter(function (elem) {
-        if (elem.title.toLowerCase().includes(techfilter)) {
-          return true;
-        }
-        return false;
-      });
+        copy = copy.filter(function (elem) {
+
+            if (elem.tech.toLowerCase().includes(techfilter)) {
+                return true;
+            }
+            return false;
+        });
     }
 
     renderProducts(copy);
